822e260982f2f4e3f5e62b00a488f12a18060ce2
ZHLHLTL <1287035721@qq.com>
2023-07-05 23:38:09 +0800

    Add files via upload

---
+++ CategoryService.java
@@ -0,0 +1,10 @@
+package cn.luozhao.travel.service;
+
+import cn.luozhao.travel.domain.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    public List<Category> findAll();
+}
